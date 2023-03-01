09a30fa321c5eeec4d0d953886b1c1dd51538433
Robinee <yb6925@naver.com>
2023-03-01 17:17:32 +0900

    Write displayAllBook func

--- app.js
+++ app.js
@@ -27,7 +27,24 @@ function addBookToLibrary() {
   //add obj to myLibrary
   myLibrary.push(book);
 }
+const test = document.querySelector('.test');
+const displayAllBtn = document.querySelector('.displayAllBooks');
 
-function displayAllBooks() {
-  //--display each book on page--
+function displayAllBook() {
+  //as soon as add a book, display the all books
+  myLibrary.forEach((book) => {
+    const { title, author, pages, read } = book;
+    //make as single card
+    const card = document.createElement('article');
+    card.classList.add('card');
+    //write down the content into card element
+    card.textContent = `title: ${title}, author: ${author}, pages: ${pages}, read: ${read}`;
+    //add to test box
+    test.appendChild(card);
+  });
 }
+
+const addBtn = document.querySelector('.add');
+
+addBtn.addEventListener('click', addBookToLibrary);
+displayAllBtn.addEventListener('click', displayAllBook);
