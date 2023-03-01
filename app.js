1dd55ce90b3de8dfde809e8b458883dfbaeb9b12
Robinee <yb6925@naver.com>
2023-03-01 15:01:03 +0900

    Write addBookToLibrary func

--- app.js
+++ app.js
@@ -12,9 +12,22 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  //push book into myLibrary array
+  //--push book into myLibrary array--
+
+  //take user input using prompt
+  //  take title, author, pages, read
+  const title = prompt('title');
+  const author = prompt('author');
+  const pages = prompt('pages');
+  const read = prompt('read');
+
+  //make input as Book obj
+  const book = new Book(title, author, pages, read);
+
+  //add obj to myLibrary
+  myLibrary.push(book);
 }
 
 function displayAllBooks() {
-  //display each book on page
+  //--display each book on page--
 }
