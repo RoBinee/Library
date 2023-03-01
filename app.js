be04f74bf6cf59ff133245985d85ad278420965a
Robinee <yb6925@naver.com>
2023-03-01 14:24:52 +0900

    Write basic functions

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
+}
+
+function addBookToLibrary() {
+  //push book into myLibrary array
+}
+
+function displayAllBooks() {
+  //display each book on page
+}
